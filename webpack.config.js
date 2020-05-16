7f218682b8436fa04e9529dc301bcbc67f0622dc
Ryan Pivovar <ryanpivovar@gmail.com>
2020-05-16 12:16:33 -0400

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,43 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  devServer: {
+    compress: true,
+    contentBase: './dist',
+    hot: true,
+    open: true,
+    port: 1235,
+    writeToDisk: true,
+  },
+  devtool: 'inline-source-map',
   entry: './src/index.tsx',
-
+  module: {
+    rules: [
+      {
+        include: path.join(__dirname, 'src'),
+        loader: 'ts-loader',
+        test: /\.ts$|tsx/,
+      }
+    ]
+  },
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].bundle.js',
     chunkFilename: '[name].chunk.js'
   },
-
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
+    plugins: [
+      new TsconfigPathsPlugin({}),
+    ],
   },
-
-  module: {
-    loaders: [
-      {
-        test: /\.ts$/,
-        include: path.join(__dirname, 'src'),
-        loader: 'ts-loader'
-      }
-    ]
-  },
-
-  devServer: {
-  contentBase: './dist'
-  }
 };
