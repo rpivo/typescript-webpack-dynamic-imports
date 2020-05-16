5204ae3d3f22923e9f4fe89a957e2f6a9432466d
Ryan Pivovar <ryanpivovar@gmail.com>
2020-05-16 12:16:49 -0400

    add index, app, header react files

---
+++ src/components/App.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [isAuthorized, setIsAuthorized] = useState(false);
+  return <div>Hello from App!</div>;
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,6 @@ import { render } from 'react-dom';
 import App from '@components/App';
 
 render(
-  <div>hello world</div>,
+  <App />,
   document.getElementById('root'),
 );
