02167346a9c13929861065a79e340f9a773f1faf
Ryan Pivovar <ryanpivovar@gmail.com>
2020-05-22 06:40:48 -0400

    update server.js with express-static-gzip

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const expressStaticGzip = require('express-static-gzip');
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
-
+app.use('/', expressStaticGzip('dist', {
+  enableBrotli: true
+ }));
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
