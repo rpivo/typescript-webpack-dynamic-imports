134818d473a7bc916888444db1216c84bd5ccf10
Ryan Pivovar <ryanpivovar@gmail.com>
2020-05-23 06:44:17 -0400

    update server to check if browser is internet explorer and conditionally serve either gz or br files depending on if IE or not

--- server.js
+++ server.js
@@ -4,7 +4,15 @@ const expressStaticGzip = require('express-static-gzip');
 const app = express();
 const port = 3000;
 
-app.use('/', expressStaticGzip('dist', {
-  enableBrotli: true
- }));
+app.use('/', (req, res, next) => {
+  const isInternetExplorer = req.headers['user-agent'] &&
+    req.headers['user-agent'].indexOf('MSIE') > -1;
+
+  const path = isInternetExplorer ? 'dist/gz' : 'dist/br';
+
+  expressStaticGzip(path, {
+    enableBrotli: isInternetExplorer ? false : true,
+  });
+});
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
