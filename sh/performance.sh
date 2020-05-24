d005e8a95c2caea22d0d16e07de16e916e55b6dd
Ryan Pivovar <ryanpivovar@gmail.com>
2020-05-24 15:49:07 -0400

    update performance.sh to run 10 lighthouse audits per metric

--- sh/performance.sh
+++ sh/performance.sh
@@ -1,2 +1,6 @@
-lighthouse http://localhost:3000/br/index.html --emulated-form-factor=none --only-categories=performance --output-path=./reports/br-$(date +"%Y-%m-%d-%I-%M-%S-%p").html --view
-lighthouse http://localhost:3000/gz/index.html --emulated-form-factor=none --only-categories=performance --output-path=./reports/gz-$(date +"%Y-%m-%d-%I-%M-%S-%p").html --view
+for run in {1..10}
+do
+lighthouse http://localhost:3000/br/index.html --emulated-form-factor=none --only-categories=performance --output=json --output-path=./reports/br-$(date +"%Y-%m-%d-%I-%M-%S-%p").json
+lighthouse http://localhost:3000/gz/index.html --emulated-form-factor=none --only-categories=performance --output=json --output-path=./reports/gz-$(date +"%Y-%m-%d-%I-%M-%S-%p").json
+done
+node report.js
