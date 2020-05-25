7c22e2ff4945c301ce4a721fae7ee2190fcd5802
Ryan Pivovar <ryanpivovar@gmail.com>
2020-05-25 07:53:56 -0400

    add log title to performance.sh

--- sh/performance.sh
+++ sh/performance.sh
@@ -3,4 +3,5 @@ do
 lighthouse http://localhost:3000/br/index.html --emulated-form-factor=none --only-categories=performance --output=json --output-path=./reports/br-$(date +"%Y-%m-%d-%I-%M-%S-%p").json
 lighthouse http://localhost:3000/gz/index.html --emulated-form-factor=none --only-categories=performance --output=json --output-path=./reports/gz-$(date +"%Y-%m-%d-%I-%M-%S-%p").json
 done
+printf "\n\nAveraged Metrics\n"
 node report.js
