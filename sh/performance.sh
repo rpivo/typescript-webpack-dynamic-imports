2da16fbbc92b0c4fa156e3d93ee6a7e02b1c6337
Ryan Pivovar <ryanpivovar@gmail.com>
2020-05-25 09:25:32 -0400

    add depth argument to performance.sh to all for arbitrary number of audits to run

--- sh/performance.sh
+++ sh/performance.sh
@@ -1,4 +1,6 @@
-for run in {1..10}
+depth=$1
+num=${depth//[!0-9]/}
+for i in $(seq ${num:-1})
 do
 lighthouse http://localhost:3000/br/index.html --emulated-form-factor=none --only-categories=performance --output=json --output-path=./reports/br-$(date +"%Y-%m-%d-%I-%M-%S-%p").json
 lighthouse http://localhost:3000/gz/index.html --emulated-form-factor=none --only-categories=performance --output=json --output-path=./reports/gz-$(date +"%Y-%m-%d-%I-%M-%S-%p").json
